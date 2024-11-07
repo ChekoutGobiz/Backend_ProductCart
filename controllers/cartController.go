fd07458396c442a5c63a9fdde0a19b0af2647a65
Dewi Kresnawati <dewidesember20@gmail.com>
2024-11-07 19:59:28 +0700

    perbaiki

--- controllers/cartController.go
+++ controllers/cartController.go
@@ -8,7 +8,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/ChekoutGobiz/ChekoutGobiz-backend/models"
+	"github.com/ChekoutGobiz/BackendChekout/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
