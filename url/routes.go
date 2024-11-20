8abc4f7abc4023272bd38bdc52f4e699f9176054
Dewi Kresnawati <dewidesember20@gmail.com>
2024-11-20 23:05:17 +0700

    ganti

--- url/routes.go
+++ url/routes.go
@@ -1,43 +1,31 @@
-// routes.go
-package routes
+package url
 
 import (
-	"net/http"
-
-	"github.com/ChekoutGobiz/BackendChekout/controller"
-	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+	controllers "github.com/ChekoutGobiz/BackendChekout/controller"
+	"github.com/gofiber/fiber/v2"
 )
 
-// SetupRoutes sets up all the routes for the application
-func SetupRoutes() http.Handler {
-	r := mux.NewRouter()
+// SetupRoutes mendefinisikan semua rute aplikasi
+func SetupRoutes(app *fiber.App) {
+	// Grouping untuk lebih rapi
+	api := app.Group("/api")
 
 	// Authentication routes
-	r.HandleFunc("/register", controllers.Register).Methods("POST")
-	r.HandleFunc("/login", controllers.Login).Methods("POST")
+	api.Post("/register", controllers.Register)
+	api.Post("/login", controllers.Login)
 
 	// Region routes
-	r.HandleFunc("/regions", controllers.CreateRegion).Methods("POST")
-	r.HandleFunc("/regions", controllers.GetRegions).Methods("GET")
+	api.Post("/regions", controllers.CreateRegion)
+	api.Get("/regions", controllers.GetRegions)
 
 	// Product routes
-	r.HandleFunc("/products", controllers.CreateProduct).Methods("POST")
-	r.HandleFunc("/products", controllers.GetProducts).Methods("GET")
-	r.HandleFunc("/products/region", controllers.GetProductsByRegion).Methods("GET")
+	api.Post("/products", controllers.CreateProduct)
+	api.Get("/products", controllers.GetProducts)
+	api.Get("/products/region", controllers.GetProductsByRegion)
 
 	// Cart routes
-	r.HandleFunc("/cart", controllers.AddToCart).Methods("POST")
-	r.HandleFunc("/cart", controllers.GetCart).Methods("GET")
-	r.HandleFunc("/cart/item", controllers.UpdateCartItem).Methods("PUT")
-	r.HandleFunc("/cart/item", controllers.RemoveCartItem).Methods("DELETE")
-
-	// Configure CORS
-	corsHandler := handlers.CORS(
-		handlers.AllowedOrigins([]string{"http://127.0.0.1:5501"}),        // Allow Live Server origin
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"}), // Allowed methods
-		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
-	)
-
-	return corsHandler(r)
+	api.Post("/cart", controllers.AddToCart)
+	api.Get("/cart", controllers.GetCart)
+	api.Put("/cart/item", controllers.UpdateCartItem)
+	api.Delete("/cart/item", controllers.RemoveCartItem)
 }
